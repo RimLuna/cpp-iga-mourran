092afe6d072b40a82591f87a1f845c2edbdec9fe
RimLuna <bougsrim@mail.com>
2022-01-10 11:42:13 +0100

    bh

--- module03/ex01/ScavTrap.hpp
+++ module03/ex01/ScavTrap.hpp
@@ -8,10 +8,10 @@ class ScavTrap : public ClapTrap
 public:
 	ScavTrap();
 	ScavTrap(const std::string &name);
-	ScavTrap(const ScavTrap &a);
+	ScavTrap(const ScavTrap &o);
 	virtual ~ScavTrap();
 
-	ScavTrap &operator=(const ScavTrap &a);
+	ScavTrap &operator=(const ScavTrap &o);
 
 	void attack(const std::string &target);
 	void guardGate();
